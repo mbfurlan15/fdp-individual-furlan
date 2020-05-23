124320b2718f8e7c9a6a2ad77f6124785150e9f6
mbfurlan15 <furlan.dmtr@gmail.com>
2020-05-23 16:46:33 -0400

    conceito de classes, metodos, if/else e cadastros

--- public/ex04-covid19/doc.md
+++ public/ex04-covid19/doc.md
@@ -5,13 +5,13 @@
     - RF01 - O usuário cadastra o Paciente (nome,sexo)
     - RF02 - O usuário informa o número de vagas totais
     - RF03 - O usuário remove o Paciente quando tiver alta
-    - RF04 - O Sistema liber mais uma vaga quando o usuaário é removido
+    - RF04 - O Sistema libera mais uma vaga quando o usuaário é removido
     - RF05 - O Sistema calcula a porcentagem da ocupação
     - RF06 - O sistema passar a cadastrar no Hospital de campanha após 70% de lotação
     - RF07 - O Usuário informa o numero de vagas do hospital de campanha
     - RF08 - O Sistema mostra a quantidade de pessoas no hospital de campanha
     - RF09 - O Usuário pode excluir do hospital da campanha.
-    - RF10 - O exibe as listas de pessoas
+    - RF10 - O Sistema exibe as listas de pessoas
 
 ## RNF - Não Funcionais
 
--- public/ex04-covid19/hospital.js
+++ public/ex04-covid19/hospital.js
@@ -14,16 +14,31 @@ class Paciente {
     }
 }
 
-
+var h1
+var h2
 function cadastrarHospital(){
    //acessar as vagas da tela
    let hosp1 = parseInt(document.getElementById("hosp1").value)
    let hosp2 = parseInt(document.getElementById("hosp2").value)
 
    //instancia dois hospitais e preenche as vagas
-   let h1 = new Hospital(hosp1,0,"N",[])
-   let h2 = new Hospital(hosp2,0,"C",[])
+   h1 = new Hospital(hosp1,0,"N",[])
+   h2 = new Hospital(hosp2,0,"C",[])
 
    console.log(h1)
    console.log(h2)
+}
+
+function adicionarPaciente(){
+    let nome = document.getElementById("pac").value
+    let sexo = document.querySelector("input[type=radio]:checked").value
+    let p = new Paciente (nome, sexo)
+    if (h1.pacientes.length<0.7*h1.vagas){
+        h1.pacientes.push(p)
+    console.log("Cadastrado em h1")}
+        else{
+            h2.pacientes.push(p)
+            console.log("Cadastrado em h2")
+        }
+    
 }
